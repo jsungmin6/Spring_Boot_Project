e077d92e6d4795aad7b7c47ca58c0ae439ec22be
jsungmin6 <jsungmin506@gmail.com>
2020-12-22 00:26:11 +0900

    src파일

---
+++ SpringBootProject/src/main/java/com/gona/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.gona.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
