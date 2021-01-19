b83f08e1c9af53fd3d770317b7b691e06e7f1f65
jsungmin6 <jsungmin506@gmail.com>
2021-01-19 18:01:32 +0900

    sping data jpa. practice query method

---
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -0,0 +1,54 @@
+package study.datajpa.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
+import study.datajpa.entitiy.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String userName, int age);
+
+    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
+
+
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name)" +"from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username = :name")
+    Member findMembers(@Param("name") String username);
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUsername(String name); //컬렉션
+    Member findMemberByUsername(String name); //단건
+    Optional<Member> findOptionalByUsername(String name); //단건 Optional
+
+    Page<Member> findByAge(int age, Pageable pageable); //count 쿼리 사용
+
+
+    Slice<Member> findBySlice(String name, Pageable pageable); //count 쿼리 사용 안함
+    List<Member> findByUsername(String name, Pageable pageable); //count 쿼리 사용 안함
+    List<Member> findByUsername(String name, Sort sort);
+
+    @Query(value = "select m from Member m", countQuery = "select count(m.username) from Member m")
+    Page<Member> findMemberAllCountBy(Pageable pageable);
+
+    @Modifying
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+}
---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,91 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entitiy.Member;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+@Transactional
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+        Member findMember =
+                memberRepository.findById(savedMember.getId()).get();
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername())
+        ;
+        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+    }
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
+    }
+
+    @Test
+    public void page() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+        //when
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC,"username"));
+        Page<Member> page = memberRepository.findByAge(10, pageRequest);
+        //then
+        List<Member> content = page.getContent(); //조회된 데이터
+        assertThat(content.size()).isEqualTo(3); //조회된 데이터 수
+        assertThat(page.getTotalElements()).isEqualTo(5); //전체 데이터 수
+        assertThat(page.getNumber()).isEqualTo(0); //페이지 번호
+        assertThat(page.getTotalPages()).isEqualTo(2); //전체 페이지 번호
+        assertThat(page.isFirst()).isTrue(); //첫번째 항목인가?
+        assertThat(page.hasNext()).isTrue(); //다음 페이지가 있는가?
+    }
+
+    @Test
+    public void bulkUpdate() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+        //when
+        int resultCount = memberRepository.bulkAgePlus(20);
+        //then
+        assertThat(resultCount).isEqualTo(3);
+    }
+}
