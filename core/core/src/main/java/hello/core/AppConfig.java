6a48ef809f23affc37ffbad37560a1d6b859d16e
jsungmin6 <jsungmin506@gmail.com>
2021-02-10 19:56:53 +0900

    OCP,DIP 적용

---
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,36 @@
+package hello.core;
+
+import hello.core.Member.MemberRepository;
+import hello.core.Member.MemberService;
+import hello.core.Member.MemberServiceImpl;
+import hello.core.Member.MemoryMemeberRepository;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemeberRepository();
+    }
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+}
--- core/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,4 +1,38 @@
+package hello.core.order;
+
+import com.sun.source.tree.AssertTree;
+import hello.core.AppConfig;
+import hello.core.Member.Grade;
+import hello.core.Member.Member;
+import hello.core.Member.MemberService;
+import hello.core.Member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class OrderServiceTest {
-  
+
+public class OrderServiceTest {
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "ItemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
 }
