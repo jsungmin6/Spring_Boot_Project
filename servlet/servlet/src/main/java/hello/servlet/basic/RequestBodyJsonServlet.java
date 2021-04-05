df9656fd2a069403ed024dc9bc26c7627429dff4
jsungmin6 <jsungmin506@gmail.com>
2021-04-05 10:44:03 +0900

    서블릿 회원생성,조회 -> jsp 변환

--- servlet/servlet/src/main/java/hello/servlet/basic/RequestBodyJsonServlet.java
+++ servlet/servlet/src/main/java/hello/servlet/basic/RequestBodyJsonServlet.java
@@ -22,7 +22,9 @@ import java.nio.charset.StandardCharsets;
 
 @WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-bodyjson")
 public class RequestBodyJsonServlet extends HttpServlet {
+
     private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse
             response)
@@ -30,8 +32,9 @@ public class RequestBodyJsonServlet extends HttpServlet {
         ServletInputStream inputStream = request.getInputStream(); String messageBody = StreamUtils.copyToString(inputStream,
                 StandardCharsets.UTF_8);
         System.out.println("messageBody = " + messageBody);
-        HelloData helloData = objectMapper.readValue(messageBody,
-                HelloData.class);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
         System.out.println("helloData.username = " + helloData.getUsername());
         System.out.println("helloData.age = " + helloData.getAge());
         response.getWriter().write("ok");
